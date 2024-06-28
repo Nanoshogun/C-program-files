b10add7b5edca98e6f32b62153bee1d680c2a27a
Okon <okoneenang@gmail.com>
2024-06-28 06:22:23 +0100

    Add files via upload

---
+++ Student Record System/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello and welcome to the Student Record System!\n");
+
+    char name[20];
+    printf("Please tell me your name: ");
+    fgets(name, 20, stdin);
+    printf("Good day %s", name);
+
+    char studentName[20];
+    int rollNum[1000];
+    double marks[1000];
+
+
+
+
+    return 0;
+}
